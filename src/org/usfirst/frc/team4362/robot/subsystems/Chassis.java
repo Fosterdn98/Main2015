7887e607f9a5eb813bdfd0cac14640727865075e
CommandMan7 <CommandMan7@gmail.com>
2015-01-21 11:59:12 -0500

    Changed Talon to CANTalon in the chassis subsytem.

--- src/org/usfirst/frc/team4362/robot/subsystems/Chassis.java
+++ src/org/usfirst/frc/team4362/robot/subsystems/Chassis.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team4362.robot.subsystems;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 import org.usfirst.frc.team4362.robot.RobotMap;
 
 /**
@@ -17,15 +19,15 @@ import org.usfirst.frc.team4362.robot.RobotMap;
 
 public class Chassis extends Subsystem {
 	
-	private Talon leftFront; /*!< Creates Talon leftFront. */
-	private Talon leftBack; /*!< Creates Talon leftBack. */
-	private Talon rightFront; /*!< Creates Talon rightFront. */
-	private Talon rightBack; /*!< Creates Talon rightBack. */
+	private CANTalon leftFront; /*!< Creates Talon leftFront. */
+	private CANTalon leftBack; /*!< Creates Talon leftBack. */
+	private CANTalon rightFront; /*!< Creates Talon rightFront. */
+	private CANTalon rightBack; /*!< Creates Talon rightBack. */
 	public Chassis(){
-	leftFront = new Talon(RobotMap.C_LEFTFRONT); /*!< Sets leftFront to a new Talon on PWM port RobotMap.C_LEFTFRONT. Needs to be changed to use TalonSRX's. */
-	leftBack = new Talon(RobotMap.C_LEFTBACK); /*!< Sets leftBack to a new Talon on PWM port RobotMap.C_LEFTBACK. Needs to be changed to use TalonSRX's. */
-	rightFront = new Talon(RobotMap.C_RIGHTFRONT); /*!< Sets rightFront to a new Talon on PWM port RobotMap.C_RIGHTFRONT. Needs to be changed to use TalonSRX's. */
-	rightBack = new Talon(RobotMap.C_RIGHTBACK); /*!< Sets rightBack to a new Talon on PWM port RobotMap.C_RIGHTBACK. Needs to be changed to use TalonSRX's. */
+	leftFront = new CANTalon(RobotMap.C_LEFTFRONT); /*!< Sets leftFront to a new Talon on PWM port RobotMap.C_LEFTFRONT. Needs to be changed to use TalonSRX's. */
+	leftBack = new CANTalon(RobotMap.C_LEFTBACK); /*!< Sets leftBack to a new Talon on PWM port RobotMap.C_LEFTBACK. Needs to be changed to use TalonSRX's. */
+	rightFront = new CANTalon(RobotMap.C_RIGHTFRONT); /*!< Sets rightFront to a new Talon on PWM port RobotMap.C_RIGHTFRONT. Needs to be changed to use TalonSRX's. */
+	rightBack = new CANTalon(RobotMap.C_RIGHTBACK); /*!< Sets rightBack to a new Talon on PWM port RobotMap.C_RIGHTBACK. Needs to be changed to use TalonSRX's. */
 	}
 	public void initDefaultCommand() {
 		/*!< This code sets the default command ran when this subsystem is called. */
