e544fed2456004f92ee24f13ba58db7d8e6d78be
CommandMan7 <CommandMan7@gmail.com>
2015-01-20 19:06:08 -0500

    Changed some of the constants, including the drive talons and shifter solenoid CAN ports.

--- src/org/usfirst/frc/team4362/robot/RobotMap.java
+++ src/org/usfirst/frc/team4362/robot/RobotMap.java
@@ -7,11 +7,12 @@ package org.usfirst.frc.team4362.robot;
  * To Do List -- add variables as necessary for the function of the robot.
  */
 public class RobotMap {
-	public static final int C_LEFTFRONT = 1; /*!< Constant value for the left front talon's CAN address. */
-	public static final int C_LEFTBACK = 3; /*!< Constant value for the left back talon's CAN address. */
-	public static final int C_RIGHTFRONT = 2; /*!< Constant value for the right front talon's CAN address. */
-	public static final int C_RIGHTBACK = 4; /*!< Constant value for the right back talon's CAN address. */
-	public static final int C_SHIFTER = 2; /*!< Constant value for the shifter solenoid's CAN address. */
+	public static final int C_LEFTFRONT = 4; /*!< Constant value for the left front talon's CAN address. */
+	public static final int C_LEFTBACK = 2; /*!< Constant value for the left back talon's CAN address. */
+	public static final int C_RIGHTFRONT = 1; /*!< Constant value for the right front talon's CAN address. */
+	public static final int C_RIGHTBACK = 3; /*!< Constant value for the right back talon's CAN address. */
+	public static final int C_SHIFTERLOW = 0; /*!< Constant value for the shifter's low gear solenoid CAN address. */
+	public static final int C_SHIFTERHIGH = 1; /*!< Constant value for the shifter's high gear solenoid CAN address. */
 	public static final int C_COMPRESSORPORT = 1; /*!< Constant value for the compressor's CAN address. */
 	public static final int C_LEFTSTICK = 0; /*!< Constant value for the left joystick's USB port. */
 	public static final int C_RIGHTSTICK = 1; /*!< Constant value for the right joysticks's USB port. */
